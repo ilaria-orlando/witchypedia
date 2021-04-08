7668b5c6ef812162ef716b5831365e3f30a2aa86
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2021-04-08 18:30:29 +0200

    Connection to MongoDB server

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,59 +3,77 @@ require("dotenv").config({
 });
 
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-
-      {
-          resolve: `gatsby-source-filesystem`,
-          options: {
-              name: `images`,
-              path: `${__dirname}/src/images`,
-          },
-      },
-
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Gatsby Default Starter`,
+        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+        author: `@gatsbyjs`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-
-      {
-          resolve: `gatsby-source-contentful`,
-          options: {
-              spaceId: process.env.SPACE_ID,
-              // Learn about environment variables: https://gatsby.dev/env-vars
-              accessToken: process.env.ACCESS_TOKEN,
-          },
-      },
-
-      `@chakra-ui/gatsby-plugin`,
-
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-image`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: process.env.SPACE_ID,
+                // Learn about environment variables: https://gatsby.dev/env-vars
+                accessToken: process.env.ACCESS_TOKEN,
+            },
+        },
+        {
+            // The name of the plugin
+            resolve: 'gatsby-source-mongodb',
+            options: {
+                // Name of the database and collection where are books reside
+                dbName: process.env.ATLAS_DB_NAME,
+                collection: process.env.ATLAS_COLLECTION,
+                server: {
+                    address: process.env.ATLAS_ADDRESS,
+                    port: process.env.ATLAS_PORT
+                },
+                auth: {
+                    user: process.env.ATLAS_USER,
+                    password: process.env.ATLAS_PASSWORD
+                },
+                extraParams: {
+                    replicaSet: 'Cluster0-shard-0',
+                    ssl: true,
+                    authSource: 'admin',
+                    retryWrites: true
+                }
+            }
+        },
+        `@chakra-ui/gatsby-plugin`,
+        `gatsby-plugin-gatsby-cloud`,
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require("path");
 
 exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions;

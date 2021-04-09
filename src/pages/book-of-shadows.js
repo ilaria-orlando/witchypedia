e5df029bb7d1a2f9d8e1b080223bf9e179ef8c08
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2021-04-09 09:55:38 +0200

    Before implementing rest api

--- src/pages/book-of-shadows.js
+++ src/pages/book-of-shadows.js
@@ -6,18 +6,20 @@ import Layout from "../components/layout";
 const BookOfShadows = (props) => {
     console.log(props);
     const notes = props.data.allMongodbWitchypediaNotes.nodes;
-
+// TODO links edit note
+// TODO link delete note
+// TODO new note
     return(
         <Layout>
             <div className="divStyle">
                 {notes.map(note =>
                     <div className="note">
-                        <Link to={'/note/' + note.id}>
                             {/* <img src={note.node.thumbnailUrl} /> */}
                             <h2>{note.title}</h2>
                             <p>{note.content}</p>
                             <h6>{note.date}</h6>
-                        </Link>
+                            <Link to={'/editNote/'+note.id}>Edit</Link>
+                            <Link to={'/deleteNote/'}>Delete</Link>
                     </div>
                 )}
             </div>

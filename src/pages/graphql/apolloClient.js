fbf1e70ec0485cac769a381a5bf09f7d95c86c47
Steeeeeph <stephanie.vanbockhaven@hotmail.com>
2021-04-21 15:23:46 +0200

    Before giving up on crud in gatsby

--- src/pages/graphql/apolloClient.js
+++ src/pages/graphql/apolloClient.js
@@ -1,7 +1,23 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
+async function loginEmailPassword(email, password) {
+  // Create an anonymous credential
+  const credentials = Realm.Credentials.emailPassword(email, password);
+  try {
+    // Authenticate the user
+    const user = await app.logIn(credentials);
+    // `App.currentUser` updates to match the logged in user
+    assert(user.id === app.currentUser.id)
+    return user
+  } catch(err) {
+    console.error("Failed to log in", err);
+  }
+}
+loginEmailPassword("joe.jasper@example.com", "passw0rd").then(user => {
+  console.log("Successfully logged in!", user)
+})
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'https://realm.mongodb.com/api/client/v2.0/app/witchypedia-wigwf/graphql',
   cache: new InMemoryCache()
 });
 
